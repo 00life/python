6be4fddc08ddc795b60b032283bf99eac43dd9bf
00life <116957019+00life@users.noreply.github.com>
2026-03-20 13:41:55 -0400

    Create autoNoisy.sh

---
+++ noisy/autoNoisy.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# Variables
+github_noisy=
+
+echo "\033[32m"
+
+func_random(){
+min=$1;
+max=$(($2-$1+1));
+x=`hexdump -n 2 -e '/2 "%u"' /dev/urandom`;
+echo $(($x%$max+$min))
+};
+
+time_sleep1=$(func_random 1 2)m;
+echo "[*] Sleeping for $time_sleep1 minutes"
+sudo sleep $time_sleep1;
+
+echo "[*] Running PyNoise"
+sudo python3 /home/pi/Automate/noisy/noisy.py --config /home/pi/Automate/noisy/config.json &
+
+time_sleep2=$(func_random 60 120)m;
+echo "[*] PyNoise Finishes in $time_sleep2 minutes";
+sudo sleep $time_sleep2;
+
+pid_python=$(ps -a|grep -i python|awk '{print $1}');
+sudo kill -9 $pid_python;
+
+echo "[*] Rebooting";
+
+echo "\033[0m";
+
+sudo init 6;
+sudo reboot;
