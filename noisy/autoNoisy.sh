c6a5fe4842589c0289a613c3b1944986dd4af379
00life <116957019+00life@users.noreply.github.com>
2026-03-20 15:10:24 -0400

    Update autoNoisy.sh

--- noisy/autoNoisy.sh
+++ noisy/autoNoisy.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 # Variables
-set -- $(mktemp) $(mktemp --suffix=".zip" ) $(mktemp) $(mktemp);
+set -- $(mktemp) $(mktemp --suffix=".zip" ) $(mktemp);
 curl -sL https://raw.githubusercontent.com/00life/python/refs/heads/master/noisy/noisy.py -o "$1";
 
 curl -sL https://github.com/00life/python/raw/refs/heads/master/noisy/config.zip -o "${2}";
 
 echo "[*] Unziping"
-sudo unzip -p $2 $4 > $3;
-echo "[*] Done"
+sudo unzip $2 $3;
+cat $3;
 
 echo "\033[32m"
 
